957559d1e583c507c1a248d2a041e7e3efc06860
Kumar Chandu <kumarchandu218@gmail.com>
2024-04-18 16:02:02 -0500

    added files

---
+++ src/main/java/com/bandiClasses/DMS/repository/DogRepository.java
@@ -0,0 +1,23 @@
+package com.bandiClasses.DMS.repository;
+
+/**
+ * @author S565500 Kumar Chandu Mallireddy
+ */
+
+
+
+
+import java.util.List;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog, Integer> {
+	
+	List<Dog> findByName(String name);
+	
+
+}
---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,13 @@
+package com.bandiClasses.DMS.repository;
+
+/**
+ * @author S565500 Kumar Chandu Mallireddy
+ */
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
